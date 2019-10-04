5ca3318ced8fd6772d1affb42aa13d9493995347
perrymacdougall <perrymacdougall@gmail.com>
2019-10-03 23:26:43 -0400

    see above

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter as Router , Route } from 'react-router-dom';
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <Landing />
-        </BrowserRouter>
-      </div>
+      <Router>
+        <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path= "/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
